adba7438c971d1c4e94e18c26632227aa6d2f77e
Administrator <42592403@qq.com>
2018-01-11 17:33:00 +0800

    git push github:https://github.com/ZeroWhat/Transport.git

--- app/src/main/java/com/whycools/transport/activity/AchatDetailsActivity.java
+++ app/src/main/java/com/whycools/transport/activity/AchatDetailsActivity.java
@@ -87,8 +87,6 @@ public class AchatDetailsActivity extends BaseActivity implements View.OnClickLi
 
     @Override
     public void initViews() {
-
-
             achat_details_layout_back= findViewById(R.id.achat_details_layout_back);
             achat_details_tv_billno= findViewById(R.id.achat_details_tv_billno);
             achat_details_tv_address= findViewById(R.id.achat_details_tv_address);
